c7a9f861a7f019d73a63175bc4c357f35868a3c3
Thai Parkerizing <administrator@syn-172-101-034-209.res.spectrum.com>
2025-07-04 14:43:30 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2703124329"
+    serviceWorkerVersion: "4221433633"
   }
 });
